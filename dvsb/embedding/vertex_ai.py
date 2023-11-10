e4e8b5ccd6f80fe278bf4f3ed16c53c84e0c95ce
TatsuyaShirakawa <lilys1204@gmail.com>
2023-11-10 11:13:23 +0900

    added VertexAI

---
+++ dvsb/embedding/vertex_ai.py
@@ -0,0 +1,25 @@
+import os
+import time
+
+import numpy as np
+import numpy.typing as npt
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+from vertexai.language_models import TextEmbeddingModel
+
+from .embedding import EMBEDDING_REGISTRY, Embedding
+
+
+@EMBEDDING_REGISTRY.register
+class VertexAITextEmbedding(Embedding):
+    def __init__(self, model_name: str = "textembedding-gecko-multilingual@001") -> None:
+        self.model_name = model_name
+        self.model = TextEmbeddingModel.from_pretrained(model_name)
+
+    def get_name(self) -> str:
+        return f"VertexAITextEmbedding-{self.model_name}"
+
+    @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+    def get_embeddings(self, texts: list[str], mode: str) -> npt.NDArray[np.float64]:
+        time.sleep(0.5)
+        embeddings = self.model.get_embeddings(texts)
+        return np.asarray([embedding.values for embedding in embeddings])
